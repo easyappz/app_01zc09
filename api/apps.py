7e70f7c0821bed4060d003310935e9485ade4851
IbragimKhaniev <newinterny@gmail.com>
2025-11-30 13:45:04 +0300

    fix: correct syntax errors in api/apps.py

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConwefig):
-    we3default_auto_field = "django.db.models.BigAutoField"
-    name = "api"3
+class ApiConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "api"
